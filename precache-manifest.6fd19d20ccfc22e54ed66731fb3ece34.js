375dfbd983443643f975c227706df5e07ea4d0d7
Siddhant <90311561+MININ1NJA@users.noreply.github.com>
2021-12-07 17:03:50 +0530

    Updates

---
+++ precache-manifest.6fd19d20ccfc22e54ed66731fb3ece34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03f1984bb4b9f232a6c2a7270e76dac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "799dd44c9cad0945eb49",
+    "url": "./static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "45f5b83cf0e91ae2ae38",
+    "url": "./static/js/2.da30f6c7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.da30f6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799dd44c9cad0945eb49",
+    "url": "./static/js/main.13a8d1de.chunk.js"
+  },
+  {
+    "revision": "d25113d24f590e04e053",
+    "url": "./static/js/runtime-main.80763978.js"
+  },
+  {
+    "revision": "fd9dd5d1efd764dbbf4773397f853f3d",
+    "url": "./static/media/profile1.fd9dd5d1.png"
+  }
+]);
